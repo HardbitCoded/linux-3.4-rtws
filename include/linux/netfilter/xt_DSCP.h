fa700f18372eea96b5685f6121d7069873c60e07
Rui Santos <rui@rui-SatelliteI7.(none)>
2013-02-04 23:31:02 +0000

    Some files were missing from the first commit

--- include/linux/netfilter/xt_DSCP.h
+++ include/linux/netfilter/xt_DSCP.h
@@ -10,17 +10,21 @@
 */
 #ifndef _XT_DSCP_TARGET_H
 #define _XT_DSCP_TARGET_H
-#include <linux/netfilter/xt_dscp.h>
+
 #include <linux/types.h>
+
+#define XT_DSCP_MASK	0xfc	/* 11111100 */
+#define XT_DSCP_SHIFT	2
+#define XT_DSCP_MAX	0x3f	/* 00111111 */
 
 /* target info */
 struct xt_DSCP_info {
-	__u8 dscp;
+    __u8 dscp;
 };
 
 struct xt_tos_target_info {
-	__u8 tos_value;
-	__u8 tos_mask;
+    __u8 tos_value;
+    __u8 tos_mask;
 };
 
 #endif /* _XT_DSCP_TARGET_H */
